2cc39320a093d85d8abc4548022226e8cd45a81e
Azraeel <51210350+TheEliteWaffle@users.noreply.github.com>
2019-07-13 10:40:42 -0500

    Added Mod_Icon.png and Improved Mod_Info.lua

---
+++ Quantum Rift Invasion/mod_info.lua
@@ -0,0 +1,27 @@
+-- do not append version of your mod to the name
+name = "Quantum Rift Invasion"
+-- you should use this naming convention for UID 
+uid = "3b78fc35-quantum-rift-invasion-00002" -- last number should be updated with a release of a new version 
+-- uid = "3b78fc35-4101-4663-991e-46bdbc9bea1f" - TODO remove old UID
+version = 2 -- always use an integer for mod version
+copyright = ""
+description = "Quantum Rift Invasion By Hikou. Quantum Rift Invasion is the Seraphim Rift Taken from the FA Campaign and made a Unit. Disclaimer: (NOT BALANCED FOR PVP)"
+author = ""
+-- you must provide path to an icon of your mod (e.g. in PNG format)
+icon = "/mods/QuantumRiftInvasion/mod_icon.png"
+-- you should provide URL to forum post or 
+-- to GitHub repo containing implementation of your mod
+url = "http://forums.faforever.com/viewtopic.php?f=41&t=17751"
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = { }
+requiresNames = { }
+conflicts = { 
+-- add UIDs of previous version of this mod to this table
+-- to prevent conflicts, e.g. 
+    "3b78fc35-quantum-rift-invasion-00001" 
+}
+before = { }
+after = { }
